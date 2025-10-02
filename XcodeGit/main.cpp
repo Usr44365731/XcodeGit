9594099f50f94081b2047eb77e5bc0fef3524340
Usr44365731 <chug.castle_7t@icloud.com>
2025-10-02 22:45:38 +0200

    Few changes

--- XcodeGit/main.cpp
+++ XcodeGit/main.cpp
@@ -1,14 +1,9 @@
-//
-//  main.cpp
-//  XcodeGit
-//
-//  Created by Kacper Kuczyński on 02/10/2025.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return EXIT_SUCCESS;
+using namespace std;
+
+int main(void) {
+
+    cout << "Hello, World!" << endl;
+    return 0;
 }
