df569ba6089857b8f1eca6489d76f138e95a3891
Usr44365731 <chug.castle_7t@icloud.com>
2025-10-02 22:36:33 +0200

    Initial Commit

---
+++ XcodeGit/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  XcodeGit
+//
+//  Created by Kacper Kuczyński on 02/10/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
